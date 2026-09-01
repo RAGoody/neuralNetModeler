39a2e0c45124ea03f59b526f2e6252799e2d104f
RAGoody <ryan.a.goodwin@gmail.com>
2026-09-01 16:41:52 -0400

    added UUID generation. fixed pointer issue. each layer is now a separate object in memory.

--- layer/layer.py
+++ layer/layer.py
@@ -1,30 +1,41 @@
-from neuron.neuron import neuron
+from neuron.neuron import Neuron
+import uuid
 
-class layer:
+
+class Layer:
     debug = False
     layer = 0   #which layer this is in depth
     neuronsPerLayerCount = 0 #how many neurons we have per layer
-    neurons = [] #each position is a neuron.
-    weights = [] #parallel to self.neurons, stores the weight of the corresponding neuron
-    bias = []
     activation = 'relu'
-    input = []
-    ouput = []
     inputSet = False
     incomingConnections = 0
     outgoingConnections = 0
+
+    def __new__(cls, *args, **kwargs):
+        # Force the creation of a completely new object instance
+        instance = super().__new__(cls)
+        return instance 
 
     def __init__(self,layer,neuronsPerLayerCount,activation,debug=False):
         """
             Sets out attributes <boolean>debug and 
             <integer>layer depth this instance is with 0 being Input and the highest number Ouput, all between Hidden.
         """
+        self.id = f'Layer-{layer}-{uuid.uuid4()}'
+        self.neurons = [] #each position is a neuron.
+        self.weights = []
+        self.bias = []
+        self.input = []
+        self.output = []
         self.debug = debug
         self.layer = layer
         self.neuronsPerLayerCount = neuronsPerLayerCount
         self.incomingConnections = neuronsPerLayerCount #placeholder for later modifications to adjust layer width.
         self._createLayer(neuronsPerLayerCount,activation)
 
+    def getId(self):
+        return self.id
+    
     def getLayerNumber(self):
         return self.layer
 
@@ -86,9 +97,9 @@ class layer:
             if (self.debug == True):
                 print(f"........Creating neuron {i}.")
 
-            self.neurons.append(neuron())
+            self.neurons.append(Neuron())
             if (self.debug == True):
-                print(self.neurons[i])
+                print(self.neurons[i].getId())
 
             self.neurons[i].setConnections(self.incomingConnections)
             self.neurons[i].setActivation(activation)
